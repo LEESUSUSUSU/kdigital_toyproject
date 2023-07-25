ce151c11e6a8008a70cec68835543336f0743128
LEESUSUSUSU <kio023@naver.com>
2023-07-25 15:59:42 +0900

    리액트 시작하기

---
+++ suyeun/welfare/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Welfare from '../src/first_name/welfare'; // Assuming the component file is in the same directory
+
+const App = () => {
+    return (
+        <div>
+            <Welfare />
+        </div>
+    );
+};
+
+export default App;
---
+++ suyeun/welfare/src/first_name/welfare.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welfare = () => {
+    return (
+        <main>
+            {/* JSX content goes here */}
+        </main>
+    );
+};
+
+export default Welfare;
