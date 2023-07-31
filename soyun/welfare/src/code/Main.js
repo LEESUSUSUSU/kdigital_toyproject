2f088e8d35e76fa6683734d6ec859cf4df757c65
marine-yang-so-yun <marina0406@naver.com>
2023-07-31 16:18:20 +0900

    frontend 작업

--- soyun/welfare/src/code/Main.js
+++ soyun/welfare/src/code/Main.js
@@ -1,54 +1,65 @@
 
-import React, {useEffect, useState} from 'react';
-
+import React, { useEffect, useState } from 'react';
+import KakaoMap from './KakaoMap';
 
 const Main = () => {
 
-    const[member1List , setMember1List] = useState([])
+  const [member1List, setMember1List] = useState(["Hello"])
+  const [member2List, setMember2List] = useState(["World"])
+  var memberLat = []
+  var memberLong = []
 
-    useEffect(() => {
-      fetch('http://localhost:8080/member2')
-        .then(response => response.json() )
-        .then(response => {
-          console.log(response)
-          setMember1List(response)
-        })
-        .catch(error => console.error('Error fetching data:', error));
-    }, []);
+  useEffect(() => {
+    fetch('http://localhost:8080/member1')
+      .then(response => response.json())
+      .then(response => {
+        setMember1List(response)
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
-    const memberPrint = (arr) => {
+  useEffect(() => {
+    fetch('http://localhost:8080/member2')
+      .then(response => response.json())
+      .then(response => {
+        setMember2List(response)
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
-      const result = []
-     
-      for (let i = 0; i <arr.length; i++)  {
-        
-        result.push(
-          <div>
-            {arr[i].mb_name}
-            {arr[i].mb_address}
-            {arr[i].mb_tel}
-          </div>
-        )
+  const memberPrint = (arr) => {
 
-      }
-      return(
-        <>
-        {result}
-        </>
-      );
+    const result = []
+
+    for (let i = 0; i < arr.length; i++) {
+
+      result.push(
+        <div>
+          {arr[i].mb_name}
+          {arr[i].mb_address}
+          {arr[i].mb_tel}
+        </div>
+      )
+
     }
-
-    return(
+    return (
       <>
-      <div>
-        {memberPrint(member1List)}
-      </div>
-      
+        {result}
       </>
-    )
+    );
+  }
+
+
+
+  return (
+    <>
     
-    
+    <KakaoMap member1List = {member1List} member2List = {member2List}/>
+    </>
+  )
+
+
 }
-    
-    
+
+
 export default Main;
